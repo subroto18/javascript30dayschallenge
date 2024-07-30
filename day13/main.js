0dd7488133682b938ca4a43cbd447189d587becc
subroto18 <subroto@revivotech.in>
2024-07-30 22:39:20 +0530

    complete day 14

---
+++ day13/main.js
@@ -0,0 +1,5 @@
+// main.js
+import { myFunction, myVariable } from "./module1.js";
+
+myFunction(); // Outputs: Hello from myFunction
+console.log(myVariable); // Outputs: 42
---
+++ day13/utils.js
@@ -0,0 +1,6 @@
+// module1.js
+export const myFunction = () => {
+  console.log("Hello from myFunction");
+};
+
+export const myVariable = 42;
